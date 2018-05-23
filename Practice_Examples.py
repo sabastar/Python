998193d75530ede52602261956fdf11bd216d6eb
sabastar <saba_s@outlook.com>
2018-05-22 19:39:11 -0700

    Update Practice_Examples.py

--- Practice_Examples.py
+++ Practice_Examples.py
@@ -6,4 +6,14 @@ def is_even(x):
   else:
     return False
 
-  
+# ******* IS_INT ************
+# Note: here a number with a decimal 0 is also an integer > ex. 7.0
+
+# define a function is_int that takes a number x
+def is_int(x):
+  # return True if number is an integer and False otherwise
+  # The difference between a number and that same number rounded is greater than zero
+  if (x-round(x)) >= 0:
+    return True
+  else:
+    return False
