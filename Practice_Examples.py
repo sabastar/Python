66f3e3c823a04c4e020e68d8e3815c3f46b8db24
sabastar <saba_s@outlook.com>
2019-02-27 11:54:11 -0800

    Update Practice_Examples.py

--- Practice_Examples.py
+++ Practice_Examples.py
@@ -70,4 +70,25 @@ def anti_vowel(text):
 
 print anti_vowel("Hey You!")
 
-# 
+# *** SCRABBLE SCORE ***
+
+# # Define a function that takes a string word and returns the equivalent score for that word
+# Dictionary with point values
+# Assumptions: input 1 word with no spaces or punctuation
+# > no score multipliers
+
+score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2, 
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3, 
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1, 
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4, 
+         "x": 8, "z": 10}
+
+def scrabble_score(word):
+  word = word.lower() #make string lower case
+  total = 0
+  for i in word:
+    total += score[i] #access value stored in dictionary
+  return total
+
+print scrabble_score("Helix")
+
