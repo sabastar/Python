cc8b4d675d078a5fae9d76e0bd26aa60d12aa7d6
sabastar <saba_s@outlook.com>
2019-03-07 14:40:06 -0800

    Update ExamStats.py

--- ExamStats.py
+++ ExamStats.py
@@ -1,3 +1,12 @@
 # MINI PROJECT: Create a program to compute stats
 
 grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+# Write a function to print out the list of grades, one element at a time
+def print_grades(grades_input):
+  
+  #iterate through list and print each item on its own line
+  for i in grades_input:
+    print i
+    
+print_grades(grades)
