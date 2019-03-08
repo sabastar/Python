d3427d53357f28d0e8cb98735154c42f813a5cfd
sabastar <saba_s@outlook.com>
2019-03-08 09:34:52 -0800

    Update ExamStats.py

--- ExamStats.py
+++ ExamStats.py
@@ -47,3 +47,11 @@ def grades_std_deviation(variance):
   return variance **0.5
 variance = grades_variance(grades)
 print grades_std_deviation(variance)
+
+# Print out the stats
+
+print print_grades(grades)
+print grades_sum(grades)
+print grades_average(grades)
+print grades_variance(grades)
+print grades_std_deviation(grades_variance(grades))
