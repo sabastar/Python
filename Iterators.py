05def8ffe0abd845b811cb4600488442bb3c7d28
sabastar <saba_s@outlook.com>
2019-03-08 11:12:01 -0800

    Update Iterators.py

--- Iterators.py
+++ Iterators.py
@@ -14,3 +14,26 @@ print my_dict.keys()
 # tuples are surrounded by () and can contain any data type
 
 print my_dict.values()
+
+# in operator
+for number in range(5):
+  print number, # comma to print each on same line
+
+d = { 
+  "name": "Eric",
+  "age": 26
+}
+
+for key in d:
+  print key, d[key],
+
+for letter in "Eric":
+  print letter,  # note the comma!
+
+# For each key in dictionary, print out the key, space, value of that key
+
+for key in my_dict:
+  print key, my_dict[key]
+  
+  
+  
