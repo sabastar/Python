e373af1431acae40e60bd9b6bbf0df2245a719e1
sabastar <saba_s@outlook.com>
2019-03-08 09:38:06 -0800

    Create Iterators.py

---
+++ Iterators.py
@@ -0,0 +1,9 @@
+# Iterators for Dictonaries
+# Create your own dictionary with 2-3 key/value pairs the print the result of calling .items()
+
+my_dict = {
+  "Name": "Sam",
+  "Age": 44
+}
+
+print my_dict.items()
