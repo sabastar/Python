0652b6cb96493dabc9050ef676dcd60d8f36adfe
sabastar <saba_s@outlook.com>
2019-04-18 11:02:28 -0700

    Update BuildingLists.py

--- BuildingLists.py
+++ BuildingLists.py
@@ -45,3 +45,15 @@ l = [i ** 2 for i in range(1, 11)]
 # Should be [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
 print l[2:9:2]
+
+# default start is 0, end is end of list, stride is 1 
+to_five = ['A', 'B', 'C', 'D', 'E']
+
+print to_five[3:] # starts at 3
+# prints ['D', 'E'] 
+
+print to_five[:2] # ends at 2
+# prints ['A', 'B']
+
+print to_five[::2] # stride is 2
+# print ['A', 'C', 'E']
