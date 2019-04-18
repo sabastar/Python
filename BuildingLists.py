75374fbd2031277843bb006c7a5071d59389c113
sabastar <saba_s@outlook.com>
2019-04-18 10:59:33 -0700

    Update BuildingLists.py

--- BuildingLists.py
+++ BuildingLists.py
@@ -33,3 +33,15 @@ print c
 
 cubes_by_four = [x ** 3 for x in range(1, 11) if ((x ** 3) % 4) == 0]
 print cubes_by_four
+
+# List Slicing 
+# Only want part of a list > slicing allows access elements of a list in a concise manner
+# Syntax: [start:end:stride]
+# start > where to start, inclusive
+# end > where to end, exclusive
+# stride > space between items in the sliced list
+
+l = [i ** 2 for i in range(1, 11)]
+# Should be [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+print l[2:9:2]
